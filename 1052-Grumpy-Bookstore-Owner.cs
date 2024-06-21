554c225a6d5f48a51ffca157b7c9744de4d44e2e
Ahmad Adel <de.ace3@gmail.com>
2024-06-21 12:11:16 +0300

    [1052] [Time Beats: 81.25%] [Memory Beats: 37.50%] - LeetPush

--- 1052-Grumpy-Bookstore-Owner.cs
+++ 1052-Grumpy-Bookstore-Owner.cs
@@ -4,8 +4,6 @@ public class Solution {
         if(windowDictLen < 0)
             return customers[0];
 
-        var totalCust = new int[windowDictLen];
-        var happyCust = new int[windowDictLen];
         var largestDiff = 0;
         var largestDiffIndex = 0;
 
@@ -21,14 +19,11 @@ public class Solution {
         }
 
         largestDiff = totalSum - happySum;
-        totalCust[0] = totalSum;
-        happyCust[0] = happySum;
-
         totalSum -= customers[0];
         if(grumpy[0] == 0)
             happySum -= customers[0];
 
-        for(int i = 1 ; i < totalCust.Length ; i++)
+        for(int i = 1 ; i < windowDictLen ; i++)
         {
             totalSum += customers[ i + minutes - 1 ];
             if(grumpy[i+ minutes - 1] == 0)
@@ -39,25 +34,19 @@ public class Solution {
                 largestDiff = totalSum - happySum;
                 largestDiffIndex = i;
             }
-            totalCust[i] = totalSum;
-            happyCust[i] = happySum;
-
             totalSum -= customers[i];
             if(grumpy[i] == 0)
                 happySum-= customers[i];
         }
 
         for(int i = largestDiffIndex ; i <= minutes-1+largestDiffIndex ; i++)
-        {
             grumpy[i] = 0;
-        }
 
         var res = 0;
         for(int i = 0 ; i < customers.Length ; i++)
-        {
             if(grumpy[i] == 0)
                 res += customers[i];
-        }
+
         return res;
     }
 }
