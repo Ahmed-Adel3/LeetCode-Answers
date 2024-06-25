69e3fd22c32df2acd77fb5ec6cdc4d3040661f08
Ahmad Adel <de.ace3@gmail.com>
2024-06-25 10:07:23 +0300

    [538] [Time Beats: 79.31%] [Memory Beats: 37.93%] - LeetPush

---
+++ 538-Convert-BST-to-Greater-Tree.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode ConvertBST(TreeNode root) {
+        var sum = 0;
+        dfs(root);
+        return root;
+
+        void dfs(TreeNode node)
+        {
+            if(node == null)
+               return;
+
+            dfs(node.right);
+            var temp = node.val;
+            node.val += sum;
+            sum+= temp;            
+            dfs(node.left);
+        }
+    }
+}
