64a2ffbb61c399f047acd9d30a239aedf46637d3
Ahmad Adel <de.ace3@gmail.com>
2024-08-08 08:51:32 +0300

    [143] [Time Beats: 95.11%] [Memory Beats: 55.15%] - LeetPush

--- 143-Reorder-List.cs
+++ 143-Reorder-List.cs
@@ -11,26 +11,63 @@
  */
 public class Solution {
     public void ReorderList(ListNode head) {
-        var pointers = new List<ListNode>();
-
-        var curr = head;
-        while(curr != null)
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null)
         {
-            pointers.Add(curr);
-            curr = curr.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        var len = pointers.Count%2==0 ? pointers.Count/2 : pointers.Count/2 +1;
-        for(int i = 0 ; i < len ; i++)
+        //reversing second array
+        var second = slow.next;
+        var prev = slow.next = null; 
+
+        while(second != null)
         {
-            var last = pointers[pointers.Count-1-i];
-            if(i == len - 1)
-                last.next = null;
-            else
-            {
-                last.next = pointers[i+1];
-                pointers[i].next = last;
-            }
+            var temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        var first = head;
+        second = prev;
+
+        while(second is not null)
+        {
+            var temp1 = first.next;
+            var temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
         }
     }
 }
+//solution with extra space ----->
+// public class Solution {
+//     public void ReorderList(ListNode head) {
+//         var pointers = new List<ListNode>();
+
+//         var curr = head;
+//         while(curr != null)
+//         {
+//             pointers.Add(curr);
+//             curr = curr.next;
+//         }
+
+//         var len = pointers.Count%2==0 ? pointers.Count/2 : pointers.Count/2 +1;
+//         for(int i = 0 ; i < len ; i++)
+//         {
+//             var last = pointers[pointers.Count-1-i];
+//             if(i == len - 1)
+//                 last.next = null;
+//             else
+//             {
+//                 last.next = pointers[i+1];
+//                 pointers[i].next = last;
+//             }
+//         }
+//     }
+// }
